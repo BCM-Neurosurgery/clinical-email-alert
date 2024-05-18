65cf5b6fd3cd0d565e918e10952235f9644a2542
Yewen Zhou <yewenzhou@berkeley.edu>
2024-05-18 13:04:49 -0500

    Update cron bash script

--- setup_cron.sh
+++ setup_cron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Directory where main.py is located
-SCRIPT_DIR="/home/yewen/BCM/TBRD/TBRD-null-pipeline/"
+SCRIPT_DIR="/home/auto/CODE/TRBD-null-pipeline/trbdv0"
 
-# Adding a cron job
-(crontab -l 2>/dev/null; echo "0 3 * * 1 python $SCRIPT_DIR/main.py") | crontab -
+# Adding a cron job to run main.py every day at 11:10 AM
+(crontab -l 2>/dev/null; echo "10 11 * * * cd $SCRIPT_DIR && /home/auto/miniconda3/bin/python main.py") | crontab -
 
-echo "Cron job set to run main.py every Monday at 3:00 AM."
+echo "Cron job set to run main.py every day at 11:10 AM."
