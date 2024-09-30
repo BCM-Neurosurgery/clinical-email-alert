10d5d530260f8852235106cba0946f44fb6952b8
Yewen Zhou <yewenzhou@berkeley.edu>
2024-09-30 09:50:41 -0500

    Update env dir

--- setup_cron.sh
+++ setup_cron.sh
@@ -4,6 +4,6 @@
 SCRIPT_DIR="/home/auto/CODE/TRBD-null-pipeline/trbdv0"
 
 # Adding a cron job to run main.py every day at 11:10 AM
-(crontab -l 2>/dev/null; echo "10 11 * * * cd $SCRIPT_DIR && /home/auto/miniconda3/bin/python main.py") | crontab -
+(crontab -l 2>/dev/null; echo "10 11 * * * cd $SCRIPT_DIR && /home/auto/miniconda3/envs/trbd-v0/bin/python main.py") | crontab -
 
 echo "Cron job set to run main.py every day at 11:10 AM."
